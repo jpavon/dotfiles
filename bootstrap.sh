c171a0c4448e77aa753909caaf7f14ef41fb25e7
Julio Pavon <julio@jpavon.com>
2013-04-12 20:01:01 +0100

    add makefile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,13 @@
-# cd ~/Projects/dotfiles
 # chmod +x bootstrap.sh
-
-# ./bootstrap.sh
-
-
 
 #!/bin/bash
 cd "$(dirname "$0")"
 
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "st2-user/" -av . ~
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" -av . ~
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
-else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt
-    fi
-fi
+doIt
+
 unset doIt
 source ~/.bash_profile
