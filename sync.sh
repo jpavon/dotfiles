5fef4e8cc5861c9050dd76f5f412b9af6bfdb8db
Julio Pavón <julio@thisisbeyond.co.uk>
2014-02-11 16:06:03 +0000

    exclude sync.sh and README.md

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")"
 
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude ".osx" -av . ~
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude ".osx" --exclude "README.md" -av . ~
 }
 
 doIt
