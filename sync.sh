8bd0f3eb5ec28e42d5b20213bfceaa5b62ce4b23
Julio Pavón <julio@thisisbeyond.co.uk>
2014-02-21 09:32:32 +0000

    Spacing

--- sync.sh
+++ sync.sh
@@ -3,9 +3,7 @@ cd "$(dirname "$0")"
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude ".osx" --exclude "README.md" -av . ~
 }
-
 doIt
-
 unset doIt
 
 # Go back to previous directory
